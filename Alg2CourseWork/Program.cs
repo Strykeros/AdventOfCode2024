21053606b8102c1aa3980544187f604fdba6b3c0
Strykeros <sampleemail@email.com>
2025-03-22 00:27:42 +0000

    addming input file

--- Alg2CourseWork/Program.cs
+++ Alg2CourseWork/Program.cs
@@ -1,33 +1,31 @@
-﻿namespace Alg2CourseWork
+﻿namespace Alg2CourseWorkDay20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filePath = "C:\\Users\\Juzer\\Desktop\\Programming\\C#\\Alg2CourseWork\\input.txt";
+            string filePath = Directory.GetCurrentDirectory() + "C:\\..\\..\\..\\input.txt";
 
-            string[] lines = File.ReadAllLines(filePath);
-            int lineCount = lines.Length;
-            int lineSymbolCount = lines[0].Length;
+            string[] lineRows = File.ReadAllLines(filePath);
+            int rowCount = lineRows.Length;
+            int colCount = lineRows[0].Length;
 
-            char[,] fileCharArray = new char[lineCount, lineSymbolCount];
+            char[,] lineCols = new char[rowCount, colCount];
             List<(int, int)> startPoint = new List<(int, int)>();
             List<(int, int)> finishPoint = new List<(int, int)>();
 
             int[][] directions = { [0, +1], [0, -1], [-1, 0], [+1, 0] };
-            int[][] directionsWithCheats = { [0, +2], [+2, 0], [0, -2], [-2, 0], [+1, +1], [-1, +1], [+1, -1], [-1, -1] };
-            //int[][] directionsWithCheats = { [0, +2], [+2, 0], [0, -2], [-2, 0] };
+            int[][] directionsWithCheats = { [0, +2], [+2, 0], [0, -2], [-2, 0] };
 
-            // Fill the char array and find start (S) and end (E) points
-            for (int i = 0; i < lineCount; i++)
+            for (int i = 0; i < rowCount; i++)
             {
-                for (int j = 0; j < lines[i].Length; j++)
+                for (int j = 0; j < colCount; j++)
                 {
-                    fileCharArray[i, j] = lines[i][j];
+                    lineCols[i, j] = lineRows[i][j];
 
-                    if (fileCharArray[i, j] == 'S')
+                    if (lineCols[i, j] == 'S')
                         startPoint.Add((i, j));
-                    if (fileCharArray[i, j] == 'E')
+                    if (lineCols[i, j] == 'E')
                         finishPoint.Add((i, j));
                 }
             }
@@ -36,17 +34,13 @@
 
             for (int i = 0; track[track.Count - 1] != finishPoint[0]; i++)
             {
-                if (track[i] == finishPoint[0])
-                    break;
-
-                // Explore all 4 directions
                 for (int j = 0; j < directions.Length; j++)
                 {
                     int xMove = track[i].Item1 + directions[j][0];
                     int yMove = track[i].Item2 + directions[j][1];
 
-                    if (xMove >= 0 && xMove < lineCount && yMove >= 0 && yMove < lineSymbolCount &&
-                        fileCharArray[xMove, yMove] != '#' &&
+                    if (xMove >= 0 && xMove < rowCount && yMove >= 0 && yMove < colCount &&
+                        lineCols[xMove, yMove] != '#' &&
                         !track.Contains((xMove, yMove)))
                     {
                         track.Add((xMove, yMove));
@@ -54,7 +48,6 @@
                 }
             }
 
-            Console.WriteLine(track.Count);
             int totalCheats = 0;
 
             for (int i = 0; i < track.Count; i++)
@@ -79,22 +72,7 @@
                 }
             }
 
-            Console.WriteLine(totalCheats);
-
-            /*Console.WriteLine("Start Points:");
-            foreach (var (x, y) in startPoint)
-            {
-                Console.WriteLine($"S at ({x}, {y})");
-            }
-
-            Console.WriteLine("\nFinish Points:");
-            foreach (var (x, y) in finishPoint)
-            {
-                Console.WriteLine($"E at ({x}, {y})");
-            }*/
-
-
-
+            Console.WriteLine("ANSWER: To save atleast 100 picoseconds you need " + totalCheats + " cheats.");
         }
     }
 }
